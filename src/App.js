d2a423f63853feb55b9a5fe83ab4c440ffe9650e
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-05-30 21:45:18 +0100

    instal bootstrap and added input

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App py-5">
+      <div className="row mb-5">
+        <div className="col-6"></div>
+        <div className="col-4">
+          <input className="w-75" type="text" />
+          <input className="w-25" type="submit" value="Search" />
+        </div>
+        <div className="col-2"></div>
+      </div>
       <h1>Dictionary</h1>
+      <div className="row ">
+        <div className="col-6"></div>
+        <p className="col-4">what word do you want to look up?</p>
+        <div className="col-2"></div>
+      </div>
     </div>
   );
 }
