ecfc2d73838d2ac1d04df0c16707db7726963f26
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-06-19 13:40:36 +0100

    style list and synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -8,11 +8,11 @@ export default function Meaning({ meanings }) {
       {meanings.definitions.map(function (definitions, index) {
         return (
           <span className="row">
-            <div className="container-definition col col-md-7" key={index}>
-              <p className="definition ">{definitions.definition}</p>
+            <ul className="container-definition col col-md-7" key={index}>
+              <li className="definition ">{definitions.definition}</li>
               <p className="example">{definitions.example}</p>
               <Synonyms synonyms={definitions.synonyms} />
-            </div>
+            </ul>
             <div className="d-none d-lg-block col-lg-1 "></div>
             <div className="d-none d-md-block col-md-4 col-lg-3 px-1 text-end"></div>
           </span>
